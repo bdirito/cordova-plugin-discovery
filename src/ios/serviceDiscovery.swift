28f80e44d07914a70a39fa87c9c5aea4435c821e
Nick Romanyshyn <spikeynick@gmail.com>
2023-01-19 13:22:42 -0700

    Got "discovery" working on iOS

--- src/ios/serviceDiscovery.swift
+++ src/ios/serviceDiscovery.swift
@@ -1,6 +1,36 @@
 import Network
 
+func processResponse(message:String)-> [String: String]
+{
+//    NSLog(@"%@", message);
+    
+    var msgLines = message.components(separatedBy: "\r");
+
+//    NSLog(@"total lines:%lu", [msgLines count]);
+
+    var data: [String: String] = [:]
+
+
+    for i in 0..<msgLines.count {
+     //   NSLog(@"working on:%@", msgLines[i]);
+        var items = msgLines[i].split(separator: ":", maxSplits: 1);
+
+        if(items.count == 2){
+            var key = items[0].trimmingCharacters(in: .whitespacesAndNewlines);
+            var value = items[1].trimmingCharacters(in: .whitespacesAndNewlines);
+            data[key] = value;
+        }
+    }
+    return data;
+
+}
+
+
+
 @objc(serviceDiscovery) class serviceDiscovery : CDVPlugin {
+    
+    
+    
     @objc(getNetworkServices:) func getNetworkServices(_ command: CDVInvokedUrlCommand) {
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
@@ -16,9 +46,18 @@ import Network
             return;
         }
         let group = NWConnectionGroup(with: multicast, using: .udp)
+        var services: [String:[String:String]] = [:]
         group.setReceiveHandler(maximumMessageSize: 16384, rejectOversizedMessages: true) { (message, content, isComplete) in
             if((content) != nil) {
                 let str = String(decoding: content!  , as: UTF8.self)
+                var data = processResponse(message: str);
+                var nt = data["NT"]
+                var ip = data["Location"]
+                if (nt == nil) || (ip == nil) || nt != command.arguments[0] as? String {
+                    return;
+                }
+                
+                services[ip!] = data;
                 print("Received message from \(String(describing: message.remoteEndpoint)) \(String(describing: str))")
             }
         }
@@ -32,9 +71,10 @@ import Network
         let deadline = DispatchTime.now() + .seconds(10)
         mainQueue.asyncAfter(deadline: deadline) {
             group.cancel()
+            let ret: [[String:String]] = services.values.compactMap { $0 };
             pluginResult = CDVPluginResult(
                 status: CDVCommandStatus_OK,
-                messageAs: ""
+                messageAs:ret
             )
             self.commandDelegate!.send(
                 pluginResult,
